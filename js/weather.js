dc5c8ac413fad2b9a17f4b9e4c3557101faa92f9
HoneyCaki <2971913952@qq.com>
2023-03-28 22:37:15 +0800

    Site updated: 2023-03-28 22:37:09

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "10234",
+      "background": "5",
+      "tmpColor": "00CEFF",
+      "tmpSize": "18",
+      "cityColor": "FF9CCA",
+      "citySize": "18",
+      "aqiColor": "FF9CCA",
+      "aqiSize": "18",
+      "weatherIconSize": "26",
+      "alertIconSize": "18",
+      "padding": "10px 10px 10px 10px",
+      "shadow": "0",
+      "language": "auto",
+      "fixed": "false",
+      "vertical": "top",
+      "horizontal": "left",
+      "key": "0e41495b32f3417d8dc5db9aff2457bf"
+    }
+  }
